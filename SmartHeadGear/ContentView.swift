a206c9d9d85c8c77077f7c4205c85c8f426aec60
joe davis <joedavis29@gmail.com>
2020-10-24 14:08:26 -0400

    Initial Commit

---
+++ SmartHeadGear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHeadGear
+//
+//  Created by Joe Davis on 10/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHeadGear/SmartHeadGearApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHeadGearApp.swift
+//  SmartHeadGear
+//
+//  Created by Joe Davis on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct SmartHeadGearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
